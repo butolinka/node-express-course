3b9c243cd9a471875a36e3a4e7f92f0f0dfd02eb
Mariia Butolina <butolinkam@gmail.com>
2024-07-30 16:00:53 -0400

    Created controllers, data base, models, connected to data base

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+
+const express = require('express');
+const app = express();
+const tasks = require('./routs/tasks');
+const connectDB = require('./db/connections');
+const port =3000;
+require('dotenv').config();
+
+const start = async()=>{
+    try{
+    await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log('the server is listening port 3000'));
+    } catch(err){
+        console.log(err);
+    }
+};
+start();
+app.use(express.json());
+app.use('/api/v1/tasks', tasks);
