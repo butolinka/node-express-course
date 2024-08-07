9e8b3f31d2926dfbc7d2945491ef211bb423863c
Mariia Butolina <butolinkam@gmail.com>
2024-08-07 18:39:33 -0400

    Revert "week 5 assingnment, created middlewears to handle errors"
    This reverts commit 995ddca073cd03e839b3bad2db38548bbfd63052.

--- 03-task-manager/starter/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const {CustomAPIError} = require('../errors/custom-error');
-const errorHandler = (err, req, res, next)=>{
-    if (err instanceof CustomAPIError){
-        return res.status(err.statusCode).json({msg: err.message});
-    }
-    return res.status(500).json({msg:"here is an error, try again"});
-};
-
-module.exports=errorHandler;
